eb6b622e56f9fcd340a516ee068f2e5f7d637d41
Şevval Bektaş <sevvalbektas5561@gmail.com>
2022-09-19 21:30:27 +0300

    added js

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
 //* ======================================================
 //*                     TODO APP
 //* ======================================================
+
+//?Selectors
+const addBtn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("PLease enter new todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+
+    //! yeni bir li elementi olusturup bunu DOM'a bas
+    createListElement(newTodo);
+    todoInput.value = "";
+  }
+});
+
+const createListElement = (newTodo) => {
+  //? yeni bir li elementi olustur ve bu elemente obje icerisindeki
+  //? id degerini ve completed class'ini ata
+  const li = document.createElement("li");
+  // li.id = newTodo.id;
+  li.setAttribute("id", newTodo.id);
+
+  //? okey ikonu olustur ve li elementine bagla
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  //? todo basligi icin bir p elementi ve yazi dugumu olusturarak li'ye bagla
+  const p = document.createElement("p");
+  const pTextNode = document.createTextNode(newTodo.text);
+  p.appendChild(pTextNode);
+  li.appendChild(p);
+
+  //? delete ikonu olustur ve li elementine bagla
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-trash");
+  li.appendChild(deleteIcon);
+
+  //? meydana gelen li elementini ul'ye child olarak ata
+  todoUl.appendChild(li);
+};
+
+//? Enter tusu ile ekleme mumkun olsun
+todoInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addBtn.click();
+  }
+});
+
+//? Baslangicta input aktif olsun
+window.onload = function () {
+  todoInput.focus();
+};
